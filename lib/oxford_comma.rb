42220bf5a300152ca9c09d5580ae66becd9e5dc0
georgetalnagi <gtalnagi@yahoo.com>
2020-10-22 16:35:54 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length >= 3
+    last = array.pop 
+    array.join(", ") + ", and #{last}"
+  elsif 
+    array.length == 2
+    array.join(" and ")
+  else  
+    array.join(", ")
+    end  
 end
+
+
